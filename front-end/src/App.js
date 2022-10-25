a410f78d3fe5984b1c548f918bde4a8f4c420f72
bls1999 <bls1999@users.noreply.github.com>
2022-10-25 04:43:10 -0400

    Add router to new home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import More from "./More";
+import Home from "./Home.js";
+import More from "./More.js";
 import Results from "./Results/Results.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -11,15 +12,7 @@ const App = (props) => {
         <main className="App-main">
           <Routes>
             {/* a route for the home page */}
-            <Route
-              path="/more"
-              default
-              element={
-                <div>
-                  <h1>Study Buddy</h1>
-                </div>
-              }
-            />
+            <Route path="/" element={<Home />} />
 
             {/* a route for more */}
             <Route path="/more" element={<More />} />
