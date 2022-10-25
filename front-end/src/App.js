79d94a09d8ed358bbc8481ae53bf15995d265133
BrandonSomoza <83843160+BrandonSomoza@users.noreply.github.com>
2022-10-25 08:15:56 -0400

    Updating router with map page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,46 @@
-import logo from './img/logo.svg';
-import './App.css';
-import More from './More';
+import "./App.css";
+import More from "./More";
+import Results from "./Results/Results.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const App = (props) => {
   return (
-    <div> 
-      <More />
+    <div className="App">
+      <Router>
+        <main className="App-main">
+          <Routes>
+            {/* a route for the home page */}
+            <Route
+              path="/"
+              default
+              element={
+                <div>
+                  <h1>Study Buddy</h1>
+                </div>
+              }
+            />
+
+            {/* a route for more */}
+            <Route path="/more" element={<More />} />
+
+            {/* a route to see list of results */}
+            <Route path="/results" element={<Results />} />
+
+            {/* a route to see list of results */}
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h1>Page Does Not Exist</h1>
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
