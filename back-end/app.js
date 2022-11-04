cd169a804c4bc02b7dfc00890c811d7bbfc51635
bls1999 <bls1999@users.noreply.github.com>
2022-11-03 20:18:17 -0400

    Linting code, formatting
    Spacing = 4
    Arrow functions only

--- back-end/app.js
+++ back-end/app.js
@@ -1,17 +1,17 @@
 // import and instantiate express
-const express = require("express"); // CommonJS import style!
-const app = express(); // instantiate an Express object
-// we will put some server logic here later...
-// export the express app we created to make it available to other modules
+const express = require('express');
+const app = express();
 
+// results routes
 var resultRoutes = require("./routes/resultRoutes");
 
 // route for HTTP GET requests to /json-example
 app.get("/", (req, res) => {
-  // send the response as JSON text to the client
-  res.json({ message: "ROOT SERVER" });
+    // send the response as JSON text to the client
+    res.json({ message: "ROOT SERVER" });
 });
 
 app.use("/results", resultRoutes);
 
+// export the express app we created to make it available to other modules
 module.exports = app;
