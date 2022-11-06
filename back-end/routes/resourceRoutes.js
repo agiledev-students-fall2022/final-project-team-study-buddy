c56d2799710644c4f1226b11d31efd755efa0dc7
Kevin Wu <sxw207@nyu.edu>
2022-11-06 10:36:36 -0500

    add is and zipcode parameters

--- back-end/routes/resourceRoutes.js
+++ back-end/routes/resourceRoutes.js
@@ -1,40 +1,66 @@
-// URL: http://localhost:3001/resource
+// Sample URL: http://localhost:3001/resource/getResource?id=2&zipcode=11201
 
 var express = require("express"),
   router = express.Router();
 
-router
-  // Add a binding to handle '/'
-  .get("/getResource", function (req, res) {
-    let zipCode = curAddress;
-    // let zipCode = res.zipCode
+router.get("/getResource", function (req, res) {
+  let resourceID = req.query.id;
+  let zipCode = req.query.zipcode;
 
-    // Get results from database with correct id
-    result = tempdb;
+  if ([undefined, null, ""].includes(resourceID)) {
+    return res.json({ message: "No ID provided" });
+  }
 
-    // Add map url
-    result.mapUrl = `https://www.google.com/maps/dir/${zipCode}/${result.address}`;
+  if ([undefined, null, ""].includes(zipCode)) {
+    return res.json({ message: "No zip code provided" });
+  }
 
-    return res.json(result);
-  });
+  // Get results from database with correct id
+  result = tempdb[resourceID];
+
+  if (result === undefined) {
+    return res.json({ message: "Invalid ID provided" });
+  }
+
+  // Add map url
+  result.mapUrl = `https://www.google.com/maps/dir/${zipCode}/${result.address}`;
+
+  return res.json(result);
+});
 
 module.exports = router;
 
-let curAddress = "11201";
-
 let tempdb = {
-  id: 1,
-  name: "Astoria Library",
-  address: "14-01 Astoria Boulevard  Astoria, NY 11102",
-  printer: 1,
-  wifi: 0,
-  study: 1,
-  description:
-    "Incididunt et cupidatat consectetur sunt ex irure aliqua. Eiusmod ea anim ea minim ut et sint dolor enim laboris tempor sit laborum exercitation. Magna et proident irure Lorem occaecat laborum voluptate ullamco aliquip.",
-  rating: 4.5,
-  comments: [
-    "PDeserunt sint dolore laborum ipsum amet irure ut fugiat officia adipisicing Lorem anim labore ut.",
-    "Sint magna nisi et aliquip ex id sunt enim.",
-    "Tempor laboris esse sunt elit id ad sunt pariatur.",
-  ],
+  1: {
+    id: 1,
+    name: "Astoria Library",
+    address: "14-01 Astoria Boulevard  Astoria, NY 11102",
+    printer: 1,
+    wifi: 0,
+    study: 1,
+    description:
+      "Incididunt et cupidatat consectetur sunt ex irure aliqua. Eiusmod ea anim ea minim ut et sint dolor enim laboris tempor sit laborum exercitation. Magna et proident irure Lorem occaecat laborum voluptate ullamco aliquip.",
+    rating: 4.5,
+    comments: [
+      "PDeserunt sint dolore laborum ipsum amet irure ut fugiat officia adipisicing Lorem anim labore ut.",
+      "Sint magna nisi et aliquip ex id sunt enim.",
+      "Tempor laboris esse sunt elit id ad sunt pariatur.",
+    ],
+  },
+  2: {
+    id: 2,
+    name: "Astoria Library",
+    address: "14-01 Astoria Boulevard  Astoria, NY 11102",
+    printer: 1,
+    wifi: 0,
+    study: 1,
+    description:
+      "Incididunt et cupidatat consectetur sunt ex irure aliqua. Eiusmod ea anim ea minim ut et sint dolor enim laboris tempor sit laborum exercitation. Magna et proident irure Lorem occaecat laborum voluptate ullamco aliquip.",
+    rating: 4.5,
+    comments: [
+      "PDeserunt sint dolore laborum ipsum amet irure ut fugiat officia adipisicing Lorem anim labore ut.",
+      "Sint magna nisi et aliquip ex id sunt enim.",
+      "Tempor laboris esse sunt elit id ad sunt pariatur.",
+    ],
+  },
 };
