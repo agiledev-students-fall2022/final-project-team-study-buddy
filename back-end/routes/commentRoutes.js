2d372a64d07ce78207b50d1002d955747e6b131f
bls1999 <bls1999@users.noreply.github.com>
2022-11-10 15:31:58 -0500

    Fix comments API, refactor

--- back-end/routes/commentRoutes.js
+++ back-end/routes/commentRoutes.js
@@ -1,63 +1,53 @@
 const express = require('express');
+const { escapeHtml } = require('markdown-it/lib/common/utils');
 router = express.Router();
 
-const devResults = [ // TODO: replace with database functionality in sprint 3
-    {
-        id: "1",
-        body: "First comment",
-        username: "Jack",
-        userId: "1",
-        parentId: null,
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-    },
-    {
-        id: "2",
-        body: "Second comment",
-        username: "John",
-        userId: "2",
-        parentId: null,
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-    },
-    {
-        id: "3",
-        body: "First comment first child",
-        username: "John",
-        userId: "2",
-        parentId: "1",
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-    },
-    {
-        id: "4",
-        body: "Second comment second child",
-        username: "John",
-        userId: "2",
-        parentId: "2",
-        createdAt: "2021-08-16T23:00:33.010+02:00",
-    },
-];
+const testData = require('../tests/testData.json');
 
-//ROUTES
-router.get('/:locationid', (req, res) => {
-    return res.json({ results: devResults });
-});
+// ROUTES
 
 // Add a binding to handle '/'
-router.get("/", (req, res) => res.status(404).json({ error: "No data requested." }));
+router.get("/", (req, res) => res.status(400).json({ error: "No data requested." }));
 
-router.post("/add", (req, res) => {
+router.get('/:resourceID', (req, res) => {
+    const lid = Number.parseInt(req.params.resourceID);
+    if ([undefined, null, ""].includes(req.params.resourceID) || lid === 0 || Number.isNaN(lid)) {
+        return res.status(400).json({message: 'Invalid resource ID.'});
+    }
 
-    let results = [];
-    devResults.forEach(comment => {
-        results.push(comment);
+    if (lid > testData.length) {
+        return res.status(404).json({message: 'No resource was found with that ID.'});
+    }
+
+    console.log(testData[lid - 1]);
+    return res.json({comments: testData[lid - 1].comments});
+});
+
+router.post("/:locationID/add", (req, res) => {
+    const lid = Number.parseInt(req.params.locationID);
+    if ([undefined, null, ""].includes(req.params.locationID) || lid === 0 || Number.isNaN(lid)) {
+        return res.status(400).json({message: 'Invalid resource ID.'});
+    } else if ([undefined, null, ''].req.body.comment) {
+        return res.status(400).json({message: 'Invalid comment.'});
+    }
+
+    if (lid > testData.length) {
+        return res.status(404).json({message: 'No resource was found with that ID.'});
+    }
+
+    pidInt = Number.parseInt(req.body.parentId);
+    pidInt = [undefined, null, ''].includes(req.body.parentId) || lid === 0 || Number.isNaN(lid) ? null : pidInt;
+
+    testData[lid].comments.push({
+        id: testData[lid].comments.length + 1,
+        body: escapeHtml(req.body.comment),
+        username: "Test User",
+        userId: "2",
+        parentId: pidInt,
+        createdAt: Date.now()
     });
-    results.push({
-        locationid: "1",
-        id: "2",
-        body: "lorem ipsun dolor",
-        username: "john_doe",
-    });
 
-    return res.json({ results: results });
+    return res.json({ comments: testData[lid].comments });
     // Return the updated new list of comments
 });
 
