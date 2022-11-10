c5bb5663ed8a98ae956faea7a868f7564e344b3a
bls1999 <bls1999@users.noreply.github.com>
2022-11-10 05:08:26 -0500

    Move test data to JSON file
    Allows for standardization of data

--- back-end/routes/resultRoutes.js
+++ back-end/routes/resultRoutes.js
@@ -1,45 +1,7 @@
 const express = require('express');
 router = express.Router();
 
-const devResults = [ // TODO: replace with database functionality in sprint 3
-    {
-        name: "Elmer Holmes Bobst Library",
-        address: "70 Washington Square S, New York, NY 10012",
-        zip: 10012,
-        description: "The Elmer Holmes Bobst Library, often referred to as simply Bobst Library or Bobst, is the main library at New York University in Manhattan, New York City.",
-        printer: 1,
-        wifi: 1,
-        study: 1,
-    },
-    {
-        name: "El Barrista Cafe",
-        address: "2154 3rd Ave, New York, NY 10035",
-        zip: 10035,
-        description: "A cafe in East Harlem offering free Wi-Fi.",
-        printer: 0,
-        wifi: 1,
-        study: 0,
-    },
-    {
-        name: "Kimmel Center for University Life",
-        address: "60 Washington Square S, New York, NY 10012",
-        zip: 10012,
-        description: "Located at the heart of the NYU campus, the Kimmel Center provides space and resources for students, faculty, staff, departments, alumni, and community organizations. Visitors have access to NYU Dining, Peet’s Coffee, the adjacent Global Center for Academic & Spiritual Life, and a breathtaking view of Washington Square Park.",
-        printer: 1,
-        wifi: 1,
-        study: 1,
-    },
-    {
-        name: "My Quiet Cafe",
-        address: "2152 3rd Ave, New York, NY 10035",
-        zip: 10035,
-        description: "Lorem ipsum yada yada... this place has Wi-Fi and a quiet study space.",
-        printer: 0,
-        wifi: 1,
-        study: 1,
-    }
-];
-  
+const testData = require('../tests/testData.json');
 
 const isValidZIP = zip => {
     const zipInt = Number.parseInt(zip);
@@ -58,7 +20,7 @@ router.get("/:zip", (req, res) => {
     const zip = parseInt(req.params.zip);
     const {wifi, printer, study} = req.params; // TO-DO: make sure this is correct
     let results = [];
-    devResults.forEach(place => { // sprint 3: query database to get stored places of matching criteria
+    testData.forEach(place => { // sprint 3: query database to get stored places of matching criteria
         if (place.zip === zip) {
             results.push(place);
         }
