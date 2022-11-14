5343b57a15cefe4ee6f54da460b37da4fa63b08f
Abigail Zhou <alz4471@nyu.edu>
2022-11-14 10:52:32 -0500

    fully integrated frontend and backend

--- back-end/routes/commentRoutes.js
+++ back-end/routes/commentRoutes.js
@@ -17,10 +17,13 @@ router.get('/:resourceID', (req, res) => {
         return res.status(400).json({message: 'Invalid resource ID.'});
     }
 
-    if (lid > testData.length) {
+    const resource = testData.filter((oneResource)=> oneResource.id === lid)
+
+    if (resource.length === 0) {
         return res.status(404).json({message: 'No resource was found with that ID.'});
     }
-    return res.json({comments: testData[lid - 1].comments});
+    console.log(resource[0].comments)
+    return res.json({comments: resource[0].comments});
 });
 
 router.post("/add", (req, res) => {
@@ -38,16 +41,17 @@ router.post("/add", (req, res) => {
     pidInt = Number.parseInt(req.body.parentId);
     pidInt = [undefined, null, ''].includes(req.body.parentId) || lid === 0 || Number.isNaN(lid) ? null : pidInt;
 
-    testData[lid].comments.push({
+    const newComment = {
         id: testData[lid].comments.length + 1,
         body: escapeHtml(req.body.comment),
         username: "Test User",
         userId: "2",
         parentId: pidInt,
         createdAt: Date.now()
-    });
+    }
+    testData[lid].comments.push(newComment);
 
-    return res.json({ comments: testData[lid].comments });
+    return res.json(newComment);
     // Return the updated new list of comments
 });
 
--- front-end/src/Comments/api.js
+++ front-end/src/Comments/api.js
@@ -1,20 +1,21 @@
 import "./Comments.css";
 import axios from 'axios';
 
-export const getComments = async (resourceID) => {
-  let res = await axios.get(`http://localhost:3001/comments/${resourceID}`)
+export const getComments = async (resourceId) => {
+  let res = await axios.get(`http://localhost:3001/comments/${resourceId}`)
+  console.log(res.data)
   return res.data;
 };
 
-export const createComment = async (resourceID, text, parentId = null) => {
-  let res = await axios.post(`http://localhost:3001/comments/${resourceID}/add`, { comment: text, parentId: parentId });
+export const createComment = async (resourceId, text, parentId = null) => {
+  let res = await axios.post(`http://localhost:3001/comments/add`, { locationID: resourceId, comment: text, parentId: parentId });
   return res.data;
 };
 
-export const updateComment = async (text) => {
-  //return { text };
+export const updateComment = async (resourceId, text, commentId) => {
+  return { text };
 };
 
-export const deleteComment = async () => {
-  //return {};
+export const deleteComment = async (resourceId, commentId) => {
+  return {};
 };
