286e82324d700bee1c6b9bcc66ba70f68c2ef102
Nico Bykhovsky <nicobykhovsky@Nicos-MBP.lan>
2022-11-17 23:25:34 -0500

    testing files (dont work yet) for data schema

--- back-end/dbSchema.js
+++ back-end/dbSchema.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server')
 const Schema = mongoose.Schema;
 
-
 const space = new Schema( {
-    _id: {type: int}
+    _id: {type: int},
     name: {type: String, unique: true, retuired: true},
     address: {type: String, required: true},
     description: {type: String},
@@ -11,15 +11,16 @@ const space = new Schema( {
         wifi : {type: Boolean},
         bathroom: {type: Boolean},
         printer: {type: Boolean}
-    }
+    },
     comments: [{
         name: {type: String}
         comment: {type: String}
-    }]
+    }],
     ratings: {
         rating: {type: int}
     }
 })
+
 
 /*
 MODEL REGISTRATION GOES HERE
