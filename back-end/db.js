1f771dacff38d6f972c15229761eac78d52d65ec
Nico Bykhovsky <nicobykhovsky@Nicos-MBP.lan>
2022-11-17 20:17:45 -0500

    Moved schemas into own file

--- back-end/db.js
+++ back-end/db.js
@@ -14,38 +14,7 @@ const UserSchema = new Schema({
 });
 */
 
-//STILL WORKING
 
-const userSchema = new Schema({
-    _id: //...
-    username : {type: String, unique: true, retuired: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    refreshToken: {type: String}
-})
-
-const space = new Schema( {
-    _id: /* ... */, 
-    name: {type: String, unique: true, retuired: true},
-    address: {type: String, required: true},
-    description: {type: String},
-    resource: {
-        wifi : {type: Boolean},
-        bathroom: {type: Boolean},
-        printer: {type: Boolean}
-    }
-})
-
-comment1.save(function (err, comment) {
-    if (err) console.log(err);
-    else console.log('fallowing comment was saved:', comment);
-});
-
-/*
-MODEL REGISTRATION GOES HERE
-*/
-module.exports = mongoose.model('User'; userSchema);
-module.exports = mongoose.model('Space'; space)
 
 // connect to db
 const atlasURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSTER_URI}/${process.env.DB_NAME}`;
---
+++ back-end/dbSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const space = new Schema( {
+    _id: {type: int}
+    name: {type: String, unique: true, retuired: true},
+    address: {type: String, required: true},
+    description: {type: String},
+    resource: {
+        wifi : {type: Boolean},
+        bathroom: {type: Boolean},
+        printer: {type: Boolean}
+    }
+    comments: [{
+        name: {type: String}
+        comment: {type: String}
+    }]
+    ratings: {
+        rating: {type: int}
+    }
+})
+
+/*
+MODEL REGISTRATION GOES HERE
+*/
+module.exports = mongoose.model('Space'; space)
