6d2605b287b1c3ec98df2627d82f703d11a655ea
Nico Bykhovsky <nicobykhovsky@Nicos-MBP.lan>
2022-11-17 19:33:17 -0500

    Early work on DB schema and controller file

--- back-end/db.js
+++ back-end/db.js
@@ -1,6 +1,7 @@
 // THIS FILE SHOULD ONLY BE IMPORTED WHEN THE SERVER STARTS.
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 /*
 SCHEMAS GO HERE
@@ -13,12 +14,37 @@ const UserSchema = new Schema({
 });
 */
 
+//STILL WORKING
+
+const userSchema = new Schema({
+    username : {type: String, unique: true, retuired: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    refreshToken: {type: String}
+
+})
+
+const record = new Schema( {
+    name: {type: String, unique: true, retuired: true},
+    address: {type: String, required: true},
+    city: {type: String, required: true},
+    resource: {
+        wifi : Boolean,
+        bathroom: Boolean,
+        printer: Boolean
+    }
+})
+
+comment1.save(function (err, comment) {
+    if (err) console.log(err);
+    else console.log('fallowing comment was saved:', comment);
+});
+
 /*
 MODEL REGISTRATION GOES HERE
-
-Example:
-mongoose.model('User', UserSchema);
 */
+module.exports = mongoose.model('User'; userSchema);
+
 
 // connect to db
 const atlasURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSTER_URI}/${process.env.DB_NAME}`;
@@ -30,7 +56,7 @@ mongoose.connect(`${atlasURL}?retryWrites=true&w=majority`)
 NOTE: To use database collections, reference the above modeled schemas in any file.
 
 Example:
-const mongoose = require('mongoose');
+const mongoose = require('brnachmongoose');
 const User = mongoose.model('User');
 User.findOne(...);
 */
